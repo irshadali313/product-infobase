217e3b9b9458981fe8b4c574a0729d99d317be3e
Mohammad Irshad Ali <85108058+irshadali313@users.noreply.github.com>
2023-07-20 13:54:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
- function store() { //stores items in the localStorage
-            var brand = document.getElementById('carBrand').value;
-            var price = document.getElementById('carPrice').value;
-            var key = document.getElementById('key').value;
-            var wheel = document.getElementById('wheel').value;
+function store() { //stores items in the localStorage
+            var productName = document.getElementById('productName').value;
+            var productPrice = document.getElementById('productPrice').value;
+            var ProductId = document.getElementById('productId').value;
+            var productDetail = document.getElementById('productDetail').value;
 
-            const car = {
-                brand: brand,
-                price: price,
-                Wheel: wheel
+            const product = {
+                ProductName: productName,
+                ProductPrice: productPrice,
+                ProductDetail: productDetail
             }
 
-            window.localStorage.setItem(key, JSON.stringify(car));
+            window.localStorage.setItem(ProductId, JSON.stringify(product));
             //converting object to string
         }
 
@@ -26,14 +26,14 @@
         }
 
         function removeItem() { //deletes item from localStorage
-            var key = document.getElementById('removeKey').value; //gets key from user
-            localStorage.removeItem(key) //passes key to the removeItem method
-            console.log("remove items");
+            var ProductId = document.getElementById('removeId').value; //gets key from user
+            localStorage.removeItem(ProductId) //passes key to the removeItem method
+            alert("Items has been removed.");
         }
 
 
         window.onload = function () { //ensures the page is loaded before functions are executed.
-            document.getElementById("carForm").onsubmit = store
+            document.getElementById("productForm").onsubmit = store
             document.getElementById("removeButton").onclick = removeItem
             document.getElementById("retrieveButton").onclick = retrieveRecords
         }
